0b990b9ad3173ec2b9cb85bb8055abc38f327342
Egor Nikitin <eganik5674@gmail.com>
2023-12-21 14:27:56 +0300

    Update class_saver.py

--- src/class_saver.py
+++ src/class_saver.py
@@ -1,8 +1,9 @@
 import json
 from abc import ABC, abstractmethod
-from src.funcs_for_save import open_json_file, write_to_json_file, open_csv_file, write_to_csv_file
+from typing import Any
 
-from config import JSON_PATH, CSV_SAVER_PATH, JSON_SAVER_PATH
+from config import CSV_SAVER_PATH, JSON_SAVER_PATH
+from src.funcs_for_save import open_csv_file, open_json_file, write_to_csv_file, write_to_json_file
 from src.utils import delete_vacancy
 
 
@@ -12,15 +13,15 @@ class Saver(ABC):
     """
 
     @abstractmethod
-    def add_vacancy(self, vacancy):
+    def add_vacancy(self, vacancy: list[dict]) -> None:
         pass
 
     @abstractmethod
-    def get_vacancies_by_salary(self, salary):
+    def get_vacancies_by_salary(self, salary: int) -> None:
         pass
 
     @abstractmethod
-    def delete_vacancy(self, vacancy):
+    def delete_vacancy(self, indicator: int) -> None:
         pass
 
 
@@ -29,22 +30,22 @@ class JSONSaver(Saver):
     Класс сохраняет файлы в формат json
     """
 
-    def add_vacancy(self, vacancy):
+    def add_vacancy(self, vacancy: Any) -> None:
         format_vacancy = json.loads(vacancy)
         vacancies = open_json_file(JSON_SAVER_PATH)
         vacancies.append(format_vacancy)
         write_to_json_file(JSON_SAVER_PATH, vacancies)
 
-    def get_vacancies_by_salary(self, salary):
+    def get_vacancies_by_salary(self, salary: int) -> Any:
         vacancies = open_json_file(JSON_SAVER_PATH)
-        sorted_vacancies = list(vacancy for vacancy in vacancies if vacancy['salary'] == salary)
+        sorted_vacancies = list(vacancy for vacancy in vacancies if vacancy["salary"] == salary)
         return sorted_vacancies
 
-    def delete_vacancy(self, indicator):
+    def delete_vacancy(self, indicator: Any) -> Any:
         vacancies = open_json_file(JSON_SAVER_PATH)
         update_vacancies = delete_vacancy(vacancies, indicator)
-        if update_vacancies == 'Index error':
-            return 'Указанный индекс превышает количество вакансий в списке или меньше 0'
+        if update_vacancies == "Index error":
+            return "Указанный индекс превышает количество вакансий в списке или меньше 0"
         write_to_json_file(JSON_SAVER_PATH, update_vacancies)
 
 
@@ -53,20 +54,20 @@ class CSVSaver(Saver):
     Класс сохраняет файлы в формат csv
     """
 
-    def add_vacancy(self, vacancy):
+    def add_vacancy(self, vacancy: Any) -> None:
         format_vacancy = json.loads(vacancy)
         vacancies = open_csv_file(CSV_SAVER_PATH)
         vacancies.append(format_vacancy)
         write_to_csv_file(CSV_SAVER_PATH, vacancies)
 
-    def get_vacancies_by_salary(self, salary):
+    def get_vacancies_by_salary(self, salary: int) -> Any:
         vacancies = open_csv_file(CSV_SAVER_PATH)
-        sorted_vacancies = list(vacancy for vacancy in vacancies if vacancy['salary'] == salary)
+        sorted_vacancies = list(vacancy for vacancy in vacancies if vacancy["salary"] == salary)
         return sorted_vacancies
 
-    def delete_vacancy(self, indicator):
+    def delete_vacancy(self, indicator: Any) -> Any:
         vacancies = open_csv_file(CSV_SAVER_PATH)
         update_vacancies = delete_vacancy(vacancies, indicator)
-        if update_vacancies == 'Index error':
-            return 'Указанный индекс превышает количество вакансий в списке или меньше 0'
+        if update_vacancies == "Index error":
+            return "Указанный индекс превышает количество вакансий в списке или меньше 0"
         write_to_csv_file(CSV_SAVER_PATH, update_vacancies)
