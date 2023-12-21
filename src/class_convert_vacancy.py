8828d07e4dca5bdab8cbeb6c680de512fd0e5f71
Egor Nikitin <eganik5674@gmail.com>
2023-12-21 14:27:41 +0300

    Update class_convert_vacancy.py

--- src/class_convert_vacancy.py
+++ src/class_convert_vacancy.py
@@ -1,4 +1,5 @@
 import json
+from typing import Any
 
 from src.class_with_vacancy import Vacancy
 
@@ -7,8 +8,9 @@ class ConvertVacancy:
     """
     Класс конвертирует данные из формиата json в объект класса Vacancy и наоборот
     """
+
     @staticmethod
-    def convert_to_object(vacancies):
+    def convert_to_object(vacancies: Any) -> None:
         """
         Метод создаёт экземпляр из вакансий и добавляет вакансии в список класса Vacancy
         """
@@ -17,7 +19,7 @@ class ConvertVacancy:
             Vacancy.append_vacancy(object_vacancy)
 
     @staticmethod
-    def convert_to_json(vacancies):
+    def convert_to_json(vacancies: list[Vacancy]) -> str:
         """
         Метод конвертирует экземпляры класса Vacancy в json-файл
         """
@@ -25,12 +27,11 @@ class ConvertVacancy:
 
         for vacancy in vacancies:
             json_vacancy = {
-                'vacancy': vacancy.vacancy_name,
-                'url': vacancy.vacancy_url,
-                'salary': vacancy.salary,
-                'description': vacancy.description
+                "vacancy": vacancy.vacancy_name,
+                "url": vacancy.vacancy_url,
+                "salary": vacancy.salary,
+                "description": vacancy.description,
             }
             json_list.append(json_vacancy)
 
         return json.dumps(json_list, ensure_ascii=False, indent=4)
-
