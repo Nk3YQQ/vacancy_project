8e8cac919aec46b203940a4d8bcc1e8e30ea9ebf
Egor Nikitin <eganik5674@gmail.com>
2023-12-21 10:43:46 +0300

    Initial funcs_for_save.py

---
+++ src/funcs_for_save.py
@@ -0,0 +1,44 @@
+import csv
+import json
+from pathlib import Path
+from typing import Any
+
+
+def open_json_file(filepath: Path):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return json.load(file)
+
+    except FileNotFoundError:
+        raise FileNotFoundError('Указанный файл не найден.')
+
+
+def write_to_json_file(filepath: Path, data: Any):
+    try:
+        with open(filepath, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+    except FileNotFoundError:
+        raise FileNotFoundError('Указанный файл не найден.')
+
+
+def open_csv_file(filepath: Path):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            return list(csv.DictReader(file))
+
+    except FileNotFoundError:
+        raise FileNotFoundError('Указанный файл не найден.')
+
+
+def write_to_csv_file(filepath: Path, data: Any):
+    try:
+        with open(filepath, 'w', encoding='utf-8') as file:
+            fieldnames = ['vacancy', 'url', 'salary', 'description']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in data:
+                writer.writerow(row)
+
+    except FileNotFoundError:
+        raise FileNotFoundError('Указанный файл не найден.')
