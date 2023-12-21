08eb5d394b0739d4c648910daf39870571b08024
Egor Nikitin <eganik5674@gmail.com>
2023-12-21 22:29:42 +0300

    Update funcs_for_save.py

--- src/funcs_for_save.py
+++ src/funcs_for_save.py
@@ -14,12 +14,8 @@ def open_json_file(filepath: Path) -> Any:
 
 
 def write_to_json_file(filepath: Path, data: Any) -> Any:
-    try:
-        with open(filepath, "w", encoding="utf-8") as file:
-            json.dump(data, file, ensure_ascii=False, indent=4)
-
-    except FileNotFoundError:
-        raise FileNotFoundError("Указанный файл не найден.")
+    with open(filepath, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
 
 def open_csv_file(filepath: Path) -> Any:
@@ -32,13 +28,19 @@ def open_csv_file(filepath: Path) -> Any:
 
 
 def write_to_csv_file(filepath: Path, data: Any) -> Any:
-    try:
-        with open(filepath, "w", encoding="utf-8") as file:
-            fieldnames = ["vacancy", "url", "salary", "description"]
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            for row in data:
-                writer.writerow(row)
+    with open(filepath, "w", encoding="utf-8") as file:
+        fieldnames = ["vacancy", "url", "salary", "description"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
 
-    except FileNotFoundError:
-        raise FileNotFoundError("Указанный файл не найден.")
+
+def is_vacancy_in_file(vacancy: dict, vacancies: Any) -> Any:
+    """
+    Функция проверяет, есть ли в файле уже такая вакансия или нет по url
+    """
+    for item in vacancies:
+        if item["url"] == vacancy["url"]:
+            return True
+    return False
