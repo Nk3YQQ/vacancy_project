1772bed87aba92589581007d648389e3d95c1074
Egor Nikitin <eganik5674@gmail.com>
2023-12-21 10:43:07 +0300

    Initial class_convert_vacancy.py

---
+++ src/class_convert_vacancy.py
@@ -0,0 +1,36 @@
+import json
+
+from src.class_with_vacancy import Vacancy
+
+
+class ConvertVacancy:
+    """
+    Класс конвертирует данные из формиата json в объект класса Vacancy и наоборот
+    """
+    @staticmethod
+    def convert_to_object(vacancies):
+        """
+        Метод создаёт экземпляр из вакансий и добавляет вакансии в список класса Vacancy
+        """
+        for vacancy in vacancies:
+            object_vacancy = Vacancy(vacancy["vacancy"], vacancy["url"], vacancy["salary"], vacancy["description"])
+            Vacancy.append_vacancy(object_vacancy)
+
+    @staticmethod
+    def convert_to_json(vacancies):
+        """
+        Метод конвертирует экземпляры класса Vacancy в json-файл
+        """
+        json_list = []
+
+        for vacancy in vacancies:
+            json_vacancy = {
+                'vacancy': vacancy.vacancy_name,
+                'url': vacancy.vacancy_url,
+                'salary': vacancy.salary,
+                'description': vacancy.description
+            }
+            json_list.append(json_vacancy)
+
+        return json.dumps(json_list, ensure_ascii=False, indent=4)
+
